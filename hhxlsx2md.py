bb77e7b882927b39eaa6781d1413e6be9423140e
kahiroka <kahiroka@users.noreply.github.com>
2026-03-31 12:01:30 +0900

    improved

--- hhxlsx2md.py
+++ hhxlsx2md.py
@@ -5,12 +5,13 @@ from openpyxl import load_workbook
 MAX_ROW = 1000
 MAX_COL = 1000
 
-def parse_excel_with_hh(xlsx_path, geometry=[0,0,1,1], workaround=[]):
+def parse_excel_with_hh(xlsx_path, geometry=[0,0,1,1], minblank=1, workaround=[]):
     wb = load_workbook(xlsx_path, data_only=True)
     for sheetname in wb.sheetnames:
         print(f"# {sheetname}")
         ws = wb[sheetname]
         data_col, data_row, header_width, header_height = geometry
+        blank = minblank
         for row in range(data_row, MAX_ROW):
             term = True
             for hdr_col in range(data_col - header_width, data_col):
@@ -20,32 +21,39 @@ def parse_excel_with_hh(xlsx_path, geometry=[0,0,1,1], workaround=[]):
                     indent = hdr_col - (data_col - header_width) + 2
                     print("#"*indent + f" {hdr}")
             if term:
-                break
+                if blank == 0:
+                    break
+                blank -= 1
+            else:
+                blank = minblank
                     
             for col in range(data_col, MAX_COL):
                 value = ws.cell(row=row, column=col).value
 
                 term = True
+                line = ""
                 for hdr_row in range(data_row - header_height, data_row):
                     hdr = ws.cell(row=hdr_row, column=col).value
                     if hdr:
                         term = False
                         indent = hdr_row - (data_row - header_height)
                         if hdr_row < data_row -1:
-                            print(" "*indent*2 + "-" + f" {hdr}")
+                            line += " "*indent*2 + "-" + f" {hdr}\n"
                         elif value or "dense" in workaround:
-                            print(" "*indent*2 + "-" + f" {hdr}: ", end="")
+                            line += " "*indent*2 + "-" + f" {hdr}: "
                 if term:
                     continue
 
                 if value or "dense" in workaround:
-                    print(f"{value}")
+                    print(f"{line}", end="")
+                    print(f"{'<br>'.join(str(value).split('\n'))}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("filename", help="file name")
     parser.add_argument("--geometry", type=str, default="2x2+1+1", help="XxY[+W+H]: default data (X=1,Y=1), header (W=1,H=1)")
     parser.add_argument("--dense", action="store_true", help="display if empty")
+    parser.add_argument("--minblank", type=int, default=1, help="minimum blank lines: default 1")
     args = parser.parse_args()
 
     tmp = args.geometry.split("+")
@@ -59,4 +67,4 @@ if __name__ == "__main__":
         print(f"geometry mismatch: {geometry}", file=sys.stderr)
         sys.exit()
 
-    parse_excel_with_hh(args.filename, geometry, workaround)
+    parse_excel_with_hh(args.filename, geometry, args.minblank, workaround)
